7218d301d0da7ce0071c07255703f3d1f5231036
Tonitruc <139448882+Tonitruc@users.noreply.github.com>
2024-03-29 23:00:48 +0300

    add resize mod
    resize app when resize terminal

--- main.c
+++ main.c
@@ -1,15 +1,62 @@
-
-
 #include <panel.h>
+#include "FILE_PANEL.h"
+#include <signal.h>
+#include <locale.h>
+#include <stdbool.h>
 
-#define INIT_BASE_WIN init_pair(1, COLOR_WHITE, COLOR_WHITE); attron(COLOR_PAIR(1)); \
+#define MIN_HEIGHT 15
+#define MIN_WIDTH 15
+
+#define AMOUNT_OF_FILE_PANEL 2
+
+FILE_PANEL* first_file_panel;
+FILE_PANEL* second_file_panel;
+
+#define INIT_BASE_WIN init_pair(1, COLOR_BLACK, COLOR_WHITE); attron(COLOR_PAIR(1)); \
 					wbkgd(stdscr, COLOR_PAIR(1)); refresh();  //Set the base style for the main window
 
+bool resize_app();
+bool init_app();
+
 int main() {
-	initscr();
-	start_color();
-	INIT_BASE_WIN
+	init_app();
+
+	while(1) {
+		if(wgetch(stdscr) == KEY_RESIZE){
+			resize_app();
+		}
+	}
 
 	getch();
 	endwin();
+}
+
+
+bool init_app() {
+	setlocale(LC_ALL, "rus");
+	initscr();
+	curs_set(false);
+	start_color();
+
+	INIT_BASE_WIN
+	if(!init_file_panel(&first_file_panel, 0) || !init_file_panel(&second_file_panel, 1))
+		return false;
+
+	show_file_panel(first_file_panel);
+	show_file_panel(second_file_panel);
+	refresh();
+
+	return true;
+}
+
+
+bool resize_app() {
+	clear();
+	if(!resize_file_panel(first_file_panel, 0) || !resize_file_panel(second_file_panel, 1))
+		return false;
+
+	refresh();
+	show_file_panel(first_file_panel);
+	show_file_panel(second_file_panel);
+	return true;
 }
